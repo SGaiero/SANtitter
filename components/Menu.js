09dcd36c7d9e28c7125da42516290ac5e2fc6443
SGaiero <santigaiero@gmail.com>
2023-02-14 22:32:40 -0300

    boton de settings lindo

--- components/Menu.js
+++ components/Menu.js
@@ -1,6 +1,17 @@
-import { Box, ListItemButton, ListItemText } from "@mui/material";
+import { Box, Link, ListItemButton, ListItemText, Menu as MenuMui, MenuItem } from "@mui/material";
+import { useState } from "react";
 
 export default function Menu(){
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return(
         <Box sx={{maxWidth: "100px"}}>
             <ListItemButton  >
@@ -16,7 +27,27 @@ export default function Menu(){
                 <ListItemText primary="Lists"/>
             </ListItemButton>
             <ListItemButton>
-                <ListItemText primary="Settings"/>
+                <ListItemText
+                    id="basic-button"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick} 
+                    primary="Settings"
+                    />
+                    <MenuMui
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <MenuItem onClick={handleClose}><Link sx={{color: "black", textDecoration:"none"}} href="/config/Profile">Profile</Link></MenuItem>
+                        <MenuItem onClick={handleClose}>Theme</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </MenuMui>
             </ListItemButton>
         </Box>
     )
---
+++ pages/config/Profile.js
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+export default function Profile(){
+    return(
+        <Box>
+            <h2>Profile</h2>
+        </Box>
+    )
+}
